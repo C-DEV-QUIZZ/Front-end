a3646a01205f5c0b77797f95f29c760778947e95
Fabrice <fabrice.Ferrere@ipilyon.net>
2021-08-18 23:16:37 +0200

    - fix bad adresse web socket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   adresseBackEnd : "http://localhost:3000/",
   adresseFrontEnd : "http://localhost:4200/",
+  adressWebSocket :"ws://localhost:3000/",
   appVersion: require('../../package.json').version,
   typeEnvironnement :"Local",
   NomSite:"Local-frontend site"
